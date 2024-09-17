3273586f0e4771f9cae28d8ec0d7d10c3eafe996
Abdoulahi LO <faarame@gmail.com>
2024-09-17 14:47:05 +0000

    add close functionnality mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-import { usePathname } from "next/navigation";
+import { Sheet, SheetContent, SheetTrigger, SheetClose } from "./ui/sheet";
+import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
 import { navLinks } from "@/data";
 import { link } from "fs";
+import { useState } from "react";
 
 const MobileNav = () => {
+  const [openSheet, setOpenSheet] = useState(false);
   const pathname = usePathname();
+  const router = useRouter();
   return (
-    <Sheet>
+    <Sheet open={openSheet} onOpenChange={setOpenSheet}>
       <SheetTrigger className="flex items-center justify-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
@@ -24,16 +27,18 @@ const MobileNav = () => {
         {/* nav */}
         <nav className="flex flex-col items-center justify-center space-y-3">
           {navLinks.map((link, index) => (
-            <Link
-              key={index}
-              className={`${
-                link.href === pathname &&
-                "text-accent border-b-2  border-b-accent"
-              } transition-colors hover:text-accent-hover capitalize w-fit`}
-              href={link.href}
-            >
-              {link.name}
-            </Link>
+            <SheetClose asChild>
+              <Link
+                key={index}
+                className={`${
+                  link.href === pathname &&
+                  "text-accent border-b-2  border-b-accent"
+                } transition-colors hover:text-accent-hover capitalize w-fit`}
+                href={link.href}
+              >
+                {link.name}
+              </Link>
+            </SheetClose>
           ))}
         </nav>
       </SheetContent>
